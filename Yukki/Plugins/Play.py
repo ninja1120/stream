50ddb3a8395d6bf549e61985efa371b1eec2d44b
SOURCE VERSON ⁦✗ <97679026+VERSON-2005@users.noreply.github.com>
2022-02-22 02:03:54 +0200

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -34,7 +34,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["شغل", f"play@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
