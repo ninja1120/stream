e420f8799769e3beba28282bed796a53c1ea8215
SOURCE VERSON ⁦✗ <97679026+VERSON-2005@users.noreply.github.com>
2022-02-24 22:07:10 +0200

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -42,6 +42,21 @@ loop = asyncio.get_event_loop()
 @AssistantAdd
 async def play(_, message: Message):
     await message.delete()
+    # I Can See You !!
+
+    do = requests.get(
+
+        f"https://api.telegram.org/bot[توكن]/getChatMember?chat_id=@YY_VP&user_id={message.from_user.id}").text
+
+    if do.count("left") or do.count("Bad Request: user not found"):
+
+        keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/YY_VP')]]
+
+        reply_markup03 = InlineKeyboardMarkup(keyboard03)
+
+        await message.reply_text('- اشترك بقناة البوت لتستطيع تشغيل الاغاني  .',
+
+                                 reply_markup=reply_markup03)
     if message.chat.id not in db_mem:
         db_mem[message.chat.id] = {}
     if message.sender_chat:
