0397fb82de8b59bd41c3adb57fabdd0274b123d9
SOURCE VERSON ⁦✗ <97679026+VERSON-2005@users.noreply.github.com>
2022-02-24 22:35:26 +0200

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -45,7 +45,7 @@ async def play(_, message: Message):
     await message.delete()
     # I Can See You !!
     do = requests.get(
-        f"https://api.telegram.org/bot[توكن]/getChatMember?chat_id=@NiNJa_SuPPoORT&user_id={message.from_user.id}").text
+        f"https://api.telegram.org/bot[5041714427:AAF_NhXqKUOO9qscStjd74BJRi3gkIefKeA]/getChatMember?chat_id=@NiNJa_SuPPoORT&user_id={message.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
         keyboard03 = [[InlineKeyboardButton("- اضغط للاشتراك .", url='https://t.me/NiNJa_SuPPoORT')]]
         reply_markup03 = InlineKeyboardMarkup(keyboard03)
